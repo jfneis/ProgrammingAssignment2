ddb7941c6be99af91af37bb72724734d10fc5089
José Filipe Neis <jfneis@gmail.com>
2015-02-22 09:50:59 -0300

    Updating comments to make the code more readable

--- cachematrix.R
+++ cachematrix.R
@@ -8,22 +8,29 @@ makeCacheMatrix <- function(x = matrix()) {
         inverse <<- NULL
     }
     get <- function() x
-    setinverse <- function(solve) inverse <<- solve
+    ## <<- operator stores the object in another enviroment
+    setinverse <- function(solve) inverse <<- solve 
     getinverse <- function() inverse
+
+    ## returns a list so the object created by this function can be called as
+    ## x$get or x$setinverse
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
 }
 
 
-## try getting the inverse matrix from cache. if not possible, calculates it
+## try getting the inverse matrix from cache.
+## if not possible, calculates it (and stores in cache)
 
 cacheSolve <- function(x, ...) {
+    ## no problem using 'inverse' variable name as the cached one is in another enviroment (<<-)
     inverse <- x$getinverse()
     if(!is.null(inverse)) {
         message("getting cached data")
         return(inverse)
     }
+    ## arriving here means the cache was not hit
     data <- x$get()
     inverse <- solve(data, ...)
     x$setinverse(inverse)
